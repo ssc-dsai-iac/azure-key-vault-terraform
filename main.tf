cf2aa91d3d46cdd4081dc5b3791e8aedc0ac0179
Pascal Beaulne <pascalbeaulne@Pascals-MacBook-Pro.local>
2022-07-19 12:56:36 -0400

    Upgraded version and got rid of all hard coded variables

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+locals {
+  resource_group_name = element(coalescelist(data.azurerm_resource_group.rgrp.*.name, azurerm_resource_group.rg.*.name, [""]), 0)
+  location            = element(coalescelist(data.azurerm_resource_group.rgrp.*.location, azurerm_resource_group.rg.*.location, [""]), 0)
+}
+
 # ---------------------------------------------------------------------------------------------------------------------
 # Azure Resource Group Creation or selection - Default is "false"
 # ---------------------------------------------------------------------------------------------------------------------
@@ -13,41 +18,50 @@ resource "azurerm_resource_group" "this" {
   tags     = var.tags
 }
 
+data "azurerm_client_config" "current" {}
+
 # ---------------------------------------------------------------------------------------------------------------------
 # Creating Azure Key Vault
 # ---------------------------------------------------------------------------------------------------------------------
-data "azurerm_client_config" "current" {}
 
 resource "azurerm_key_vault" "this" {
-  name                      = var.name
-  location                  = var.location
-  resource_group_name       = var.resource_group_name
-  sku_name                  = var.sku_name
-  tenant_id                 = data.azurerm_client_config.current.tenant_id
-  soft_delete_enabled       = true
-  purge_protection_enabled  = true
-  tags                      = var.tags
-
-  network_acls {
-    default_action = "Deny"
-    bypass = "AzureServices"
-  }
+  name                            = var.name
+  location                        = local.location
+  resource_group_name             = local.resource_group_name
+  tenant_id                       = data.azurerm_client_config.current.tenant_id
+  sku_name                        = var.sku_name
+  enabled_for_deployment          = var.enabled_for_deployment
+  enabled_for_disk_encryption     = var.enabled_for_disk_encryption
+  enabled_for_template_deployment = var.enabled_for_template_deployment
+  soft_delete_retention_days      = var.soft_delete_retention_days
+  enable_rbac_authorization       = var.enable_rbac_authorization
+  purge_protection_enabled        = var.enable_purge_protection
   
-  # This will add the current logged in az user
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
+  tags                            = var.tags
 
-    key_permissions = [
-      "Get",
-    ]
+}
 
-    secret_permissions = [
-      "get", "backup", "delete", "list", "purge", "recover", "restore", "set",
-    ]
+# ---------------------------------------------------------------------------------------------------------------------
+# Creating Azure Key Vault Access Policy
+# ---------------------------------------------------------------------------------------------------------------------
+# Will be created based on the value of the "create_current_user_access_policy" variable
 
-    storage_permissions = [
-      "Get",
-    ]
-  }
+resource "azurerm_key_vault_access_policy" "this" {
+  count = var.create_current_user_access_policy ? 1 : 0
+
+  key_vault_id = azurerm_key_vault.this.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
+
+  key_permissions = [
+    "Get",
+  ]
+
+  secret_permissions = [
+    "Get", "Backup", "Delete", "List", "Purge", "Recover", "Restore", "Set",
+  ]
+
+  storage_permissions = [
+    "Get",
+  ]
 }
--- variables.tf
+++ variables.tf
@@ -4,6 +4,36 @@ variable "create_resource_group" {
   default     = false
 }
 
+variable "enabled_for_deployment" {
+  description = "Boolean flag to specify whether Azure Virtual Machines are permitted to retrieve certificates stored as secrets from the key vault"
+  type        = bool
+  default     = false
+}
+
+variable "enabled_for_disk_encryption" {
+  description = "Boolean flag to specify whether Azure Disk Encryption is permitted to retrieve secrets from the vault and unwrap keys"
+  type        = bool
+  default     = false
+}
+
+variable "enabled_for_template_deployment" {
+  description = "Boolean flag to specify whether Azure Resource Manager is permitted to retrieve secrets from the key vault"
+  type        = bool
+  default     = false
+}
+
+variable "enable_purge_protection" {
+  description = "Boolean flag whether Purge Protection is enabled for this Key Vault"
+  type        = bool
+  default     = false
+}
+
+variable "enable_rbac_authorization" {
+  description = "Boolean flag to specify whether Azure Key Vault uses Role Based Access Control (RBAC) for authorization of data actions"
+  type        = bool
+  default     = false
+}
+
 variable "location" {
   description = "The location of the Key Vault (and/or Resource Group) to create in"
   type        = string
@@ -26,6 +56,12 @@ variable "sku_name" {
   description = "The Name of the SKU used for this Key Vault. Possible values are standard and premium."
   type        = string
   default     = "standard"  
+}
+
+variable "soft_delete_retention_days" {
+  description = "The number of days that items should be retained for once soft-deleted. This value can be between 7 and 90"
+  type = number
+  default = 7
 }
 
 variable "tags" {
